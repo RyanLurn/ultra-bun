15ae7204f29f7cfbb276d98553227c66c5772d48
Ryan Luong <lumintriet@gmail.com>
2026-04-11 21:29:32 +0700

    feat(fs): define readFileContent function Result.

--- packages/fs/src/read-file/content.ts
+++ packages/fs/src/read-file/content.ts
@@ -1,4 +1,13 @@
+import type { Result } from "@repo/core/types/result";
+
+import {
+  createFallbackError,
+  type FallBackError,
+} from "@repo/core/error/create-fallback";
+
 import type { FilePath } from "@/types";
+
+import { FileDoesNotExistError } from "@/errors/file-does-not-exist";
 
 export async function readFileContent({
   format = "text",
@@ -6,8 +15,48 @@ export async function readFileContent({
 }: {
   format?: "text";
   path: FilePath;
-}) {
-  const file = Bun.file(path);
-  const content = await file[format]();
-  return content;
+}): Promise<Result<string, FileDoesNotExistError | FallBackError>> {
+  try {
+    const file = Bun.file(path);
+
+    if (await file.exists()) {
+      const content = await file[format]();
+      return {
+        success: true,
+        data: content,
+      };
+    }
+
+    const error = new FileDoesNotExistError(
+      `Cannot read file's ${format} content because the file doesn't exist`,
+      {
+        context: {
+          operation: "readFileContent",
+          path,
+        },
+      }
+    );
+    return {
+      success: false,
+      error,
+    };
+  } catch (error) {
+    const context = {
+      arguments: {
+        format,
+        path,
+      },
+      operation: "readFileContent",
+    };
+
+    const fallbackError = createFallbackError({
+      message: "Failed to read file content",
+      cause: error,
+      context,
+    });
+    return {
+      error: fallbackError,
+      success: false,
+    };
+  }
 }
