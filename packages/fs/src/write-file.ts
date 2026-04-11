b380b52b1f2a30a352dfcb0aa855fc9423940801
Ryan Luong <lumintriet@gmail.com>
2026-04-11 20:18:54 +0700

    feat(fs): add default error handling and define writeFile function's Result type.

--- packages/fs/src/write-file.ts
+++ packages/fs/src/write-file.ts
@@ -1,4 +1,10 @@
 import type { PathLike, BlobPart, BunFile, Archive, S3File } from "bun";
+import type { Result } from "@repo/core/types/result";
+
+import {
+  createFallbackError,
+  type FallBackError,
+} from "@repo/core/error/create-fallback";
 
 export async function writeFile({
   destination,
@@ -12,7 +18,30 @@ export async function writeFile({
     | string
     | Blob;
   destination: PathLike | BunFile | S3File;
-}) {
-  const bytes = await Bun.write(destination, input);
-  return bytes;
+}): Promise<Result<number, FallBackError>> {
+  try {
+    const bytes = await Bun.write(destination, input);
+    return {
+      success: true,
+      data: bytes,
+    };
+  } catch (error) {
+    const context = {
+      arguments: {
+        destination,
+        input,
+      },
+      operation: "writeFile",
+    };
+
+    const fallbackError = createFallbackError({
+      message: "Failed to write file to destination",
+      cause: error,
+      context,
+    });
+    return {
+      error: fallbackError,
+      success: false,
+    };
+  }
 }
