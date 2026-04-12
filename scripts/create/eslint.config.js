15495a18b4df0647d272ce3dd8d91769c4613159
Ryan Luong <lumintriet@gmail.com>
2026-04-12 09:44:54 +0700

    refactor(root): remove create script.

--- scripts/create/eslint.config.js
+++
@@ -1,5 +0,0 @@
-// @ts-check
-
-import { bunConfig } from "@repo/eslint-config/bun";
-
-export default bunConfig;
--- scripts/create/src/index.ts
+++
@@ -1,8 +0,0 @@
-import z from "zod";
-
-const FlagsSchema = z.object({
-  type: z.enum(["lib", "api", "spa", "ssr"]).default("lib"),
-  scope: z.string().min(1).default("repo"),
-  name: z.string().min(1),
-});
-type Flags = z.infer<typeof FlagsSchema>;
