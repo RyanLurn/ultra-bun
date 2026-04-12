bf07cade6f430bb9a6534bd09b835b49f82b7207
Ryan Luong <lumintriet@gmail.com>
2026-04-12 10:30:58 +0700

    feat(fetch/url): define createUrlObject function's Result type.

--- packages/fetch/src/url.ts
+++ packages/fetch/src/url.ts
@@ -1,4 +1,34 @@
-export function createUrlObject({ base, url }: { base?: string; url: string }) {
-  const urlObject = new URL(url, base);
-  return urlObject;
+import type { Result } from "@repo/core/types/result";
+
+import { InvalidUrlError } from "@/errors/invalid-url";
+
+export function createUrlObject({
+  base,
+  url,
+}: {
+  base?: string;
+  url: string;
+}): Result<URL, InvalidUrlError> {
+  try {
+    const urlObject = new URL(url, base);
+    return {
+      data: urlObject,
+      success: true,
+    };
+  } catch (cause) {
+    const error = new InvalidUrlError({
+      context: {
+        arguments: {
+          base,
+          url,
+        },
+        operation: createUrlObject.name,
+      },
+      cause: cause as TypeError,
+    });
+    return {
+      success: false,
+      error,
+    };
+  }
 }
