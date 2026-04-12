4ef0099dae923aede53319e020040c375f775a9f
Ryan Luong <lumintriet@gmail.com>
2026-04-12 08:30:36 +0700

    feat(json): assert caught error in jsonParse function to be SyntaxError.

--- packages/json/src/parse.ts
+++ packages/json/src/parse.ts
@@ -1,12 +1,5 @@
 import type { Result } from "@repo/core/types/result";
 
-import {
-  createFallbackError,
-  type FallBackError,
-} from "@repo/core/error/create-fallback";
-
-import type { InvalidJsonErrorContext } from "@/errors/invalid-json";
-
 import { InvalidJsonError } from "@/errors/invalid-json";
 
 export function jsonParse({
@@ -15,30 +8,23 @@ export function jsonParse({
 }: {
   reviver?: (this: unknown, key: string, value: unknown) => unknown;
   text: string;
-}): Result<unknown, InvalidJsonError | FallBackError> {
+}): Result<unknown, InvalidJsonError> {
   try {
     const value = JSON.parse(text, reviver) as unknown;
     return { success: true, data: value };
   } catch (error) {
-    const context: InvalidJsonErrorContext = {
-      arguments: {
-        reviver,
-        text,
+    const invalidJsonError = new InvalidJsonError({
+      context: {
+        arguments: {
+          reviver,
+          text,
+        },
+        operation: "jsonParse",
       },
-      operation: "jsonParse",
-    };
-
-    if (error instanceof SyntaxError) {
-      const invalidJsonError = new InvalidJsonError({ cause: error, context });
-      return {
-        error: invalidJsonError,
-        success: false,
-      };
-    }
-
-    const fallbackError = createFallbackError({ cause: error, context });
+      cause: error as SyntaxError,
+    });
     return {
-      error: fallbackError,
+      error: invalidJsonError,
       success: false,
     };
   }
