5f2937e1455ba76c15de922f4789fce9fe9284b0
Ryan Luong <lumintriet@gmail.com>
2026-04-12 21:26:30 +0700

    feat(core/json): handle cases where value is undefined or a function in jsonStringify.

--- packages/core/src/json/stringify.ts
+++ packages/core/src/json/stringify.ts
@@ -1,4 +1,12 @@
 export function jsonStringify({ value }: { value: unknown }) {
+  if (value === undefined) {
+    return "undefined";
+  }
+
+  if (typeof value === "function") {
+    return value.toString();
+  }
+
   const jsonString = JSON.stringify(value);
   return jsonString;
 }
