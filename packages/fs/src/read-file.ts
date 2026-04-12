4471802002a17f5d04a65b84ea7382a4112f4b58
Ryan Luong <lumintriet@gmail.com>
2026-04-12 08:56:01 +0700

    feat(fs): define readFileContentAsText function.

---
+++ packages/fs/src/read-file.ts
@@ -0,0 +1,59 @@
+import type { Result } from "@repo/core/types/result";
+
+import {
+  createFallbackError,
+  type FallBackError,
+} from "@repo/core/error/create-fallback";
+
+import type { FilePath } from "@/types";
+
+import { FileDoesNotExistError } from "@/errors/file-does-not-exist";
+
+export async function readFileContentAsText({
+  path,
+}: {
+  path: FilePath;
+}): Promise<Result<unknown, FileDoesNotExistError | FallBackError>> {
+  try {
+    const file = Bun.file(path);
+
+    if (await file.exists()) {
+      const content = await file.text();
+      return {
+        success: true,
+        data: content,
+      };
+    }
+
+    const error = new FileDoesNotExistError(
+      `Couldn't read text content of the requested file because the file doesn't exist`,
+      {
+        context: {
+          operation: readFileContentAsText.name,
+          path,
+        },
+      }
+    );
+    return {
+      success: false,
+      error,
+    };
+  } catch (error) {
+    const context = {
+      operation: readFileContentAsText.name,
+      arguments: {
+        path,
+      },
+    };
+
+    const fallbackError = createFallbackError({
+      message: "Failed to read file content due to unknown exception",
+      cause: error,
+      context,
+    });
+    return {
+      error: fallbackError,
+      success: false,
+    };
+  }
+}
