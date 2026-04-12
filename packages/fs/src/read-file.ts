d01faf658704397cdf5f6beb595bbaf81318c4d5
Ryan Luong <lumintriet@gmail.com>
2026-04-12 09:32:30 +0700

    feat(fs): customize the error returned by jsonParse in readFileContentAsJson.

--- packages/fs/src/read-file.ts
+++ packages/fs/src/read-file.ts
@@ -1,15 +1,82 @@
-import type { InvalidJsonError } from "@repo/json/errors/invalid-json";
 import type { Result } from "@repo/core/types/result";
 
 import {
   createFallbackError,
   type FallBackError,
 } from "@repo/core/error/create-fallback";
+import { InvalidJsonError } from "@repo/json/errors/invalid-json";
 import { jsonParse } from "@repo/json/parse";
 
 import type { FilePath } from "@/types";
 
 import { FileDoesNotExistError } from "@/errors/file-does-not-exist";
+
+export async function readFileContentAsJson({
+  path,
+}: {
+  path: FilePath;
+}): Promise<
+  Result<unknown, FileDoesNotExistError | InvalidJsonError | FallBackError>
+> {
+  try {
+    const file = Bun.file(path);
+
+    if (await file.exists()) {
+      const text = await file.text();
+      const jsonParseResult = jsonParse({ text });
+
+      if (jsonParseResult.success) {
+        return jsonParseResult;
+      }
+
+      const error = new InvalidJsonError({
+        context: {
+          ...jsonParseResult.error.context,
+          operation: readFileContentAsJson.name,
+        },
+        message:
+          "Failed to read file content as json because it doesn't conform to the JSON grammar",
+        cause: jsonParseResult.error.cause,
+      });
+      return {
+        success: false,
+        error,
+      };
+    }
+
+    const error = new FileDoesNotExistError(
+      `Failed to read file content as json because the file doesn't exist`,
+      {
+        context: {
+          operation: readFileContentAsJson.name,
+          path,
+        },
+      }
+    );
+    return {
+      success: false,
+      error,
+    };
+  } catch (cause) {
+    const context = {
+      operation: readFileContentAsJson.name,
+      arguments: {
+        path,
+      },
+    };
+
+    const error = createFallbackError({
+      message:
+        "Failed to read file content as json due to an unknown exception",
+      context,
+      cause,
+    });
+    return {
+      success: false,
+      error,
+    };
+  }
+}
 
 export async function readFileContentAsText({
   path,
@@ -51,55 +118,6 @@ export async function readFileContentAsText({
     const error = createFallbackError({
       message:
         "Failed to read file content as text due to an unknown exception",
-      context,
-      cause,
-    });
-    return {
-      success: false,
-      error,
-    };
-  }
-}
-
-export async function readFileContentAsJson({
-  path,
-}: {
-  path: FilePath;
-}): Promise<
-  Result<unknown, FileDoesNotExistError | InvalidJsonError | FallBackError>
-> {
-  try {
-    const file = Bun.file(path);
-
-    if (await file.exists()) {
-      const text = await file.text();
-      return jsonParse({ text });
-    }
-
-    const error = new FileDoesNotExistError(
-      `Failed to read file content as json because the file doesn't exist`,
-      {
-        context: {
-          operation: readFileContentAsJson.name,
-          path,
-        },
-      }
-    );
-    return {
-      success: false,
-      error,
-    };
-  } catch (cause) {
-    const context = {
-      operation: readFileContentAsJson.name,
-      arguments: {
-        path,
-      },
-    };
-
-    const error = createFallbackError({
-      message:
-        "Failed to read file content as json due to an unknown exception",
       context,
       cause,
     });
