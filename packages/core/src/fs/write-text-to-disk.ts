d8e2290cb80a80b8d949f66672c5bf4c44363fa3
Ryan Luong <lumintriet@gmail.com>
2026-04-13 09:31:56 +0700

    feat(core/fs): define writeTextToDisk function's Result type.

--- packages/core/src/fs/write-text-to-disk.ts
+++ packages/core/src/fs/write-text-to-disk.ts
@@ -1,4 +1,10 @@
 import type { AbsolutePath } from "@/types/path";
+import type { Result } from "@/types/result";
+
+import {
+  createFallbackError,
+  type FallBackError,
+} from "@/error/create-fallback";
 
 export async function writeTextToDisk({
   text,
@@ -6,7 +12,27 @@ export async function writeTextToDisk({
 }: {
   text: string;
   path: AbsolutePath;
-}) {
-  const bytesWritten = await Bun.write(path, text);
-  return bytesWritten;
+}): Promise<Result<number, FallBackError>> {
+  try {
+    const bytesWritten = await Bun.write(path, text);
+    return {
+      success: true,
+      data: bytesWritten,
+    };
+  } catch (cause) {
+    return {
+      success: false,
+      error: createFallbackError({
+        message: "Failed to write text content to disk",
+        context: {
+          operation: writeTextToDisk.name,
+          arguments: {
+            text,
+            path,
+          },
+        },
+        cause,
+      }),
+    };
+  }
 }
