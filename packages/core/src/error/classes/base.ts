c81f00e1f4bee65ea1089fe51a6597ed01b0cc9d
Ryan Luong <lumintriet@gmail.com>
2026-04-14 10:53:29 +0700

    feat(core/error): rewrite BaseError class.

--- packages/core/src/error/classes/base.ts
+++ packages/core/src/error/classes/base.ts
@@ -1,20 +1,30 @@
-export class BaseError extends Error {
-  code: string;
-  context?: Record<string, unknown>;
+type BaseContext = Record<string, unknown>;
+
+export class BaseError<
+  TCode extends string,
+  TContext extends BaseContext = never,
+  TCause = unknown,
+> extends Error {
+  code: TCode;
+  context?: TContext;
+  declare cause?: TCause;
 
   constructor({
+    name,
     message,
     code,
     context,
     cause,
   }: {
+    name: string;
     message: string;
-    code: string;
-    context?: Record<string, unknown>;
-    cause?: unknown;
+    code: TCode;
+    context?: TContext;
+    cause?: TCause;
   }) {
     super(message, { cause });
-    this.context = context;
+    this.name = name;
     this.code = code;
+    this.context = context;
   }
 }
