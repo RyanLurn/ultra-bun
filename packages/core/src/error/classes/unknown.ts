1cba9e4f52ab910934668362f584d2eda448affd
Ryan Luong <lumintriet@gmail.com>
2026-04-14 11:28:54 +0700

    feat(core/error): rewrite UnknownError class.

--- packages/core/src/error/classes/unknown.ts
+++ packages/core/src/error/classes/unknown.ts
@@ -1,20 +1,30 @@
+import type { JsonSerializableValue } from "@/types/json-serializable-value";
+
 import { BaseError } from "@/error/classes/base";
 
-export class UnknownError extends BaseError {
-  declare code: "UNKNOWN_ERROR";
-  declare cause: unknown;
+type UnknownContext = {
+  operation: string;
+  arguments: JsonSerializableValue;
+};
 
+export class UnknownError extends BaseError<
+  "UNKNOWN_ERROR",
+  UnknownContext,
+  unknown
+> {
   constructor({
     message,
     context,
     cause,
   }: {
-    message: string;
-    context?: Record<string, unknown>;
+    message?: string;
+    context: UnknownContext;
     cause: unknown;
   }) {
     super({
-      message,
+      name: "UnknownError",
+      message:
+        message ?? `An unknown value of type "${typeof cause}" was thrown`,
       code: "UNKNOWN_ERROR",
       context,
       cause,
