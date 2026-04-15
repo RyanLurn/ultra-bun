17f1de9d75af1a63cc3d8fff8c03d0c4f471e4b1
Ryan Luong <lumintriet@gmail.com>
2026-04-15 08:56:02 +0700

    feat(core/fs): define verifyFile function.

--- packages/core/src/types/fs.ts
+++ packages/core/src/types/fs.ts
@@ -1,3 +1,6 @@
+import type { BunFile } from "bun";
+
 import type { Branded } from "@/types/branded";
 
 export type ExistentPath = Branded<string, "ExistentPath">;
+export type ExistentFile = Branded<BunFile, "ExistentFile">;
