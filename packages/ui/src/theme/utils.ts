b942c049b2caa88dd812a0f3d03702c2b8367c78
Ryan Luong <lumintriet@gmail.com>
2026-04-17 08:52:45 +0700

    feat(ui/theme): define theme schemas.

---
+++ packages/ui/src/theme/utils.ts
@@ -0,0 +1,9 @@
+import z from "zod";
+
+export const UserThemeSchema = z
+  .enum(["light", "dark", "system"])
+  .catch("system");
+export const AppThemeSchema = z.enum(["light", "dark"]).catch("light");
+
+export type UserTheme = z.infer<typeof UserThemeSchema>;
+export type AppTheme = z.infer<typeof AppThemeSchema>;
