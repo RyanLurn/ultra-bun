58061ba67a417ac494f9f2c341d7d7878a271eaa
Ryan Luong <lumintriet@gmail.com>
2026-04-17 08:54:24 +0700

    feat(ui/theme): define util functions to handle storing theme in localStorage.

--- packages/ui/src/theme/utils.ts
+++ packages/ui/src/theme/utils.ts
@@ -7,3 +7,32 @@ export const AppThemeSchema = z.enum(["light", "dark"]).catch("light");
 
 export type UserTheme = z.infer<typeof UserThemeSchema>;
 export type AppTheme = z.infer<typeof AppThemeSchema>;
+
+export const THEME_STORAGE_KEY = "ui-theme";
+
+export function getStoredTheme() {
+  if (typeof window === "undefined") {
+    return "system";
+  }
+
+  try {
+    const storedTheme = localStorage.getItem(THEME_STORAGE_KEY);
+    const theme = UserThemeSchema.parse(storedTheme);
+    return theme;
+  } catch {
+    return "system";
+  }
+}
+
+export function setStoredTheme({ theme }: { theme: UserTheme }) {
+  if (typeof window === "undefined") {
+    return;
+  }
+
+  try {
+    const validTheme = UserThemeSchema.parse(theme);
+    localStorage.setItem(THEME_STORAGE_KEY, validTheme);
+  } catch {
+    return;
+  }
+}
