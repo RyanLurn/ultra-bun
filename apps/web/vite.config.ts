ce170850e4ce2100b099b2ba21d65e99058c5376
Ryan Luong <lumintriet@gmail.com>
2026-04-17 10:13:03 +0700

    build(web): define Vite config and run dev.

---
+++ apps/web/vite.config.ts
@@ -0,0 +1,30 @@
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import { WEB_APP_PORT } from "@repo/core/constants/ports";
+import tailwindcss from "@tailwindcss/vite";
+import babel from "@rolldown/plugin-babel";
+import { defineConfig } from "vite";
+import { resolve } from "node:path";
+
+export default defineConfig({
+  plugins: [
+    // Make sure that '@tanstack/react-start/plugin/vite' is passed before '@vitejs/plugin-react'
+    tanstackStart({
+      router: {
+        quoteStyle: "double",
+        semicolons: true,
+      },
+    }),
+    babel({ presets: [reactCompilerPreset()] }),
+    react(),
+    tailwindcss(),
+  ],
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"),
+    },
+  },
+  server: {
+    port: WEB_APP_PORT,
+  },
+});
