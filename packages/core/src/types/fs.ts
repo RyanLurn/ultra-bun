fcd45ee215f1a57a0224c7d27e7b61bcd285c8a8
Ryan Luong <lumintriet@gmail.com>
2026-04-18 09:34:18 +0700

    feat(core): remove unused code.

--- packages/core/src/types/fs.ts
+++
@@ -1,6 +0,0 @@
-import type { BunFile } from "bun";
-
-import type { Branded } from "@/types/branded";
-
-export type ExistentPath = Branded<string, "ExistentPath">;
-export type ExistentFile = Branded<BunFile, "ExistentFile">;
