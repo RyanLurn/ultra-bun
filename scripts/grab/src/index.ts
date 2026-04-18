2a9d654ac26bf58d3ac8ae755ff62ca1369f2a58
Ryan Luong <lumintriet@gmail.com>
2026-04-18 08:23:24 +0700

    feat(grab): add basic error handling.

--- scripts/grab/src/index.ts
+++ scripts/grab/src/index.ts
@@ -6,36 +6,51 @@ import { getFilePaths } from "@/utils/get-file-paths";
 import { processFile } from "@/utils/process-file";
 import { OUT_DIR } from "@/constants";
 
-const workspaces = await getWorkspaces();
+try {
+  await main();
 
-const selectedWorkspace = await select({
-  message: "Which workspace would you like to grab?",
-  options: workspaces.map(({ name, path }) => ({
-    value: path,
-    label: name,
-    hint: path,
-  })),
-});
-
-if (isCancel(selectedWorkspace)) {
-  cancel("Operation cancelled.");
   process.exit(0);
+} catch (error) {
+  if (error instanceof Error) {
+    log.error(error.message);
+  }
+  log.error("Something went wrong.");
+
+  process.exit(1);
 }
 
-log.step("Getting files from Git...");
-const filePaths = await getFilePaths({ dir: selectedWorkspace });
+async function main() {
+  const workspaces = await getWorkspaces();
 
-let outputContent = "";
+  const selectedWorkspace = await select({
+    message: "Which workspace would you like to grab?",
+    options: workspaces.map(({ name, path }) => ({
+      value: path,
+      label: name,
+      hint: path,
+    })),
+  });
 
-log.step("Processing files...");
-for (const path of filePaths) {
-  const content = await processFile({ path });
+  if (isCancel(selectedWorkspace)) {
+    cancel("Operation cancelled.");
+    process.exit(0);
+  }
 
-  outputContent += content;
-  outputContent += "\n";
+  log.step("Getting files from Git...");
+  const filePaths = await getFilePaths({ dir: selectedWorkspace });
+
+  let outputContent = "";
+
+  log.step("Processing files...");
+  for (const path of filePaths) {
+    const content = await processFile({ path });
+
+    outputContent += content;
+    outputContent += "\n";
+  }
+
+  const outputFile = `${parse(selectedWorkspace).name}.md`;
+  await Bun.write(join(OUT_DIR, `${outputFile}`), outputContent);
+
+  log.success(`Grabbed files and saved them in ${outputFile}`);
 }
-
-const outputFile = `${parse(selectedWorkspace).name}.md`;
-await Bun.write(join(OUT_DIR, `${outputFile}`), outputContent);
-
-log.success(`Grabbed files and saved them in ${outputFile}`);
