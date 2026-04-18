58af0e1d1840c2cb8ecf3ebac38ad90a19a7866b
Ryan Luong <lumintriet@gmail.com>
2026-04-18 08:35:59 +0700

    feat(grab): processFile now accepts ignore glob pattern.

--- scripts/grab/src/utils/process-file.ts
+++ scripts/grab/src/utils/process-file.ts
@@ -8,16 +8,37 @@ const ignoredExts = [".lock", ".svg"];
 
 const migrationMetadataGlob = new Glob("**/migrations/meta/*.json");
 
-export async function processFile({ path }: { path: string }) {
+export async function processFile({
+  path,
+  ignorePatterns = [],
+}: {
+  path: string;
+  ignorePatterns?: Glob[];
+}) {
   const parsedPath = parse(path);
   const ext = parsedPath.ext;
 
+  let isIgnored = false;
+
+  for (const pattern of ignorePatterns) {
+    if (pattern.match(path)) {
+      isIgnored = true;
+      break;
+    }
+  }
+
+  if (isIgnored === false) {
+    if (
+      ignoredExts.includes(ext) ||
+      ignoredBases.includes(parsedPath.base) ||
+      migrationMetadataGlob.match(path)
+    ) {
+      isIgnored = true;
+    }
+  }
+
   let content: string;
-  if (
-    ignoredExts.includes(ext) ||
-    ignoredBases.includes(parsedPath.base) ||
-    migrationMetadataGlob.match(path)
-  ) {
+  if (isIgnored) {
     content = "[SKIPPED]";
   } else {
     content = await Bun.file(path).text();
