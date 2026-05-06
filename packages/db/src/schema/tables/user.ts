8ad91e69068ec83ed2915da20adc744c1c6dac4e
Ryan Luong <lumintriet@gmail.com>
2026-05-06 11:03:33 +0700

    feat(db/constants): create USER_ROLES constant.

--- packages/db/src/schema/tables/user.ts
+++ packages/db/src/schema/tables/user.ts
@@ -4,12 +4,11 @@ import type { UserId } from "@/types/ids";
 
 import { timestampsWithDelete } from "@/schema/helpers/timestamps";
 import { id } from "@/schema/helpers/id";
+import { USER_ROLES } from "@/constants";
 
 export const userTable = pgTable("users", {
   id: id.$type<UserId>(),
-  role: text("role", { enum: ["user", "staff", "admin"] })
-    .notNull()
-    .default("user"),
+  role: text("role", { enum: USER_ROLES }).notNull().default("user"),
   name: text("name").notNull(),
   email: text("email").notNull().unique(),
   emailVerified: boolean("email_verified").notNull(),
