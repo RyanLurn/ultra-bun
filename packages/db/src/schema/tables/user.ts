4827664cd3f57c6f3170805ee272002ba392badd
Ryan Luong <lumintriet@gmail.com>
2026-05-06 09:58:23 +0700

    feat(db/schema): add role column to userTable.

--- packages/db/src/schema/tables/user.ts
+++ packages/db/src/schema/tables/user.ts
@@ -7,6 +7,9 @@ import { id } from "@/schema/helpers/id";
 
 export const userTable = pgTable("users", {
   id: id.$type<UserId>(),
+  role: text("role", { enum: ["user", "staff", "admin"] })
+    .notNull()
+    .default("user"),
   name: text("name").notNull(),
   email: text("email").notNull().unique(),
   emailVerified: boolean("email_verified").notNull(),
